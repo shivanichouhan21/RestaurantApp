b8d5a13949576250089cb292f21d9d8df4bd2e7e
Shivani Chouhan <shivani.c@cisinlabs.com>
2022-05-05 18:23:58 +0530

    intail app

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path')
+
+var app = express();
+
+
+
+app.use(express.static(__dirname+'/dist'));
+
+
+
+var server = app.listen(process.env.PORT ||8080, function () {
+    // var host = server.address().address
+    var port =process.env.PORT ||8080
+
+    console.log("Example app listening at http://%s:%s", port)
+})
+
+app.get("/*",(req,res)=>{
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+})
